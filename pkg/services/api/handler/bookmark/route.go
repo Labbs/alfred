1bab87585bcc4ae1cb3f0f5158f3384869958853
Labbs <moutonjeremy@labbs.fr>
2022-10-23 23:36:09 +0200

    add create bookmark function

--- pkg/services/api/handler/bookmark/route.go
+++ pkg/services/api/handler/bookmark/route.go
@@ -3,15 +3,17 @@ package bookmark
 import (
 	"github.com/gofiber/fiber/v2"
 	b "github.com/labbs/alfred/pkg/services/bookmark"
+	u "github.com/labbs/alfred/pkg/services/user"
 )
 
 type bookmarkHander struct {
-	bookmkark b.BookmarkRepository
+	bookmark b.BookmarkRepository
+	user     u.UserRepository
 }
 
 func InitRoute(r fiber.Router) {
-	h := bookmarkHander{bookmkark: b.NewBookmarkRepository()}
+	h := bookmarkHander{bookmark: b.NewBookmarkRepository(), user: u.NewUserRepository()}
 
 	g := r.Group("/bookmark")
-	g.Post("/Create")
+	g.Post("/create", h.createBookmark)
 }
