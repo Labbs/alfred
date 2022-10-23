abf6d8a6042115ec49d1a2b37d69b80685d679c7
Labbs <moutonjeremy@labbs.fr>
2022-10-23 22:25:01 +0200

    add token queries

--- pkg/services/user/repository.go
+++ pkg/services/user/repository.go
@@ -16,7 +16,7 @@ func NewUserRepository() UserRepositoryDB {
 
 func (d UserRepositoryDB) FindUserByUsername(username string) (User, *exception.AppError) {
 	u := User{}
-	r := d.client.DB.Where("username = ?", username).First(&u)
+	r := d.client.DB.Preload("Tokens").Where("username = ?", username).First(&u)
 	if r.Error != nil {
 		return User{}, exception.NewUnexpectedError("unable to find user", r.Error)
 	}
@@ -27,6 +27,31 @@ func (d UserRepositoryDB) UpdateUser(user User) *exception.AppError {
 	r := d.client.DB.Save(&user)
 	if r.Error != nil {
 		return exception.NewUnexpectedError("unable to update user", r.Error)
+	}
+	return nil
+}
+
+func (d UserRepositoryDB) CreateToken(token Token) *exception.AppError {
+	r := d.client.DB.Create(&token)
+	if r.Error != nil {
+		return exception.NewUnexpectedError("unable to create token", r.Error)
+	}
+	return nil
+}
+
+func (d UserRepositoryDB) FindTokenById(id string) (Token, *exception.AppError) {
+	t := Token{}
+	r := d.client.DB.Where("id = ?", id).First(&t)
+	if r.Error != nil {
+		return Token{}, exception.NewUnexpectedError("unable to find token", r.Error)
+	}
+	return t, nil
+}
+
+func (d UserRepositoryDB) DeleteTokenById(id string) *exception.AppError {
+	r := d.client.DB.Delete(&Token{}, id)
+	if r.Error != nil {
+		return exception.NewUnexpectedError("unable to delete token", r.Error)
 	}
 	return nil
 }
